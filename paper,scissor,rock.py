f8a5f9566b1534ecbb23c2e0b21a17d6020213d6
tracine3636 <39446050+tracine3636@users.noreply.github.com>
2019-09-11 16:01:52 -0400

    Add files via upload

---
+++ paper,scissor,rock.py
@@ -0,0 +1,53 @@
+from os import system
+import random
+import time
+
+List =["Paper","Scissors","Rock"]
+
+
+query = input("Hello, would you like to play paper, scissors, rock? yes or no?  ")
+
+if query ==("yes" or "y"): 
+        print("Okay, lets play!")
+        
+
+print('\n')
+
+time.sleep(1)
+
+print("Ready")
+
+time.sleep(.5)
+print('\n')
+print("3")
+
+time.sleep(.5)
+print('\n')
+print("2")
+
+time.sleep(.5)
+print('\n')
+print("1")
+
+
+
+
+
+print('\n')
+
+time.sleep(1.5)
+
+print(random.choice(List)) 
+
+
+
+
+	
+
+
+
+
+
+
+
+        
